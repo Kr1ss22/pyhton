dcf3b3ac8a4a7888922bda9477c7b13950472adc
Kr1ss22 <kristjan.mustkivi@hkhk.edu.ee>
2024-01-17 12:53:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-tekst = (input ("Lisa tekst : "))
+kasutaja_arv = int(input("Sisesta arv: "))
 
-
-
-
-
+if kasutaja_arv == 0:
+    print("Sisestasid nulli. Palun sisesta mõni muu arv.")
+elif kasutaja_arv % 2 == 0:
+    print("Sisestatud arv on paaris.")
+else:
+    print("Sisestatud arv on paaritu.")
