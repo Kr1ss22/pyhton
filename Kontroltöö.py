c5869c69915c3c75a38447c996e08a5efdecdfa8
Kr1ss22 <kristjan.mustkivi@hkhk.edu.ee>
2024-02-06 20:16:26 +0200

    valmis (peaaegu)

--- Kontroltöö.py
+++ Kontroltöö.py
@@ -77,9 +77,120 @@ elif kasutaja_arv % 2 == 0:
 else:
     print("Sisestatud arv on paaritu.")
 
+# 8. TÃ¤ringud
+# 	kuvatakse korrektne arusaadav kÃ¼simus ja hiljem vastus - 1p
+# 	kasutaja vÃµistleb kahe tÃ¤ringuga arvuti vastu - 1p
+# 	kasutaja teeb pakkumise ning suurima tÃ¤ringupunktisumma vÃµitja saab laual oleva raha endale - 2p
+# 	kood kommenteeritud - 1p
+    
+    def taringumang():
+     print("Tere tulemast täringumängu!")
+     panus = int(input("Palun sisestage oma panus: "))
+     print("Teie panus on ", panus, " eurot")
+     print("arvuti viskab täringuid...")
+     ab = random.randint(1,6)
+     ac = random.randint(1,6)
+     taring = ab+ac
+     vastus = int(input("Arvake ära mitu tärni arvuti viskas: "))
+     raha = panus*2
+     if taring==vastus:
+         print("Teie võitsite! Te võitsite ", raha, " eurot")
+     else:
+        print("Arvuti võitis, õige vastus oli", taring, "! Te kaotasite ", panus, " eurot")
 
+    taringumang()
+    
+    # 10. KaugushÃ¼pe
+	# kasutaja sisestab 3 kaugushÃ¼ppe tulemust - 1p
+	# sinu programm leiab ning vÃ¤ljastab parima ja keskmise tulemuse - 2p
+	# programmi dialoog kasutajaga on arusaadav ja Ã¼heselt mÃµistetav - 1p
+	# kood kommenteeritud - 1p
 
+    def kaugushupe():
+     tulemused = []
+     for i in range(3):
+         tulemus = float(input("Sisesta kaugushüppe tulemus: "))
+         tulemused.append(tulemus)
+     print("Parim tulemus on ", max(tulemused), "m")
+     print("Keskmine tulemus on ", sum(tulemused)/len(tulemused), "m")
+    kaugushupe()
 
+# 12. Eurokalkulaator
+# 	Koosta programm, mis kalkuleerib valuuta vastavalt kasutaja soovile EUR->EEK vÃµi EEK->EUR.
+# 	Oluline on kasutada kahte funktsiooni!!
+    
+    def eur_eek():
+        summa = int(input("Sisestage summa eurodes: "))
+        print(summa*15.6466, "krooni")
+    def eek_eur():
+    summa = int(input("Sisestage summa kroonides: "))
+print(summa/15.6466, "eur")
 
+valik = input("Kas soovite teisendada Eurosid[EUR] või Kroone[EEK]: ")
+if valik == "EUR":
+                    eur_eek()
+                elif valik == "EEK":
+                    eek_eur()
+                else:    
+                        print("Vale valik!")
 
+    # 14 Palkade vÃµrdlus - Loo palk.txt fail tÃ¶Ã¶tajate nime, soo ja palganumbriga (10 tÃ¶Ã¶tajat).
+	#Koosta programm, mis analÃ¼Ã¼sib kas firmas toimub diskrimineerimist soo jÃ¤rgi. Selleks vÃµrdle omavahel meeste ja naiste palkade keskmiseid, samuti meeste ja naiste kÃµige kÃµrgemat palka. Programm peab tegema otsuse.
+     
+        def palgad():
+         fail = open("palgad.txt", encoding="UTF-8")
+            mehed = []
+            naised = []
+            for rida in fail:
+                osad = rida.split()
+                if osad[2] == "m":
+                    mehed.append(int(osad[3]))
+                else:
+                    naised.append(int(osad[3]))
+            fail.close()
+            meeste_keskmine = sum(mehed)/len(mehed)
+            meeste_max = max(mehed)
+            naiste_keskmine = sum(naised)/len(naised)
+            naiste_max = max(naised)
+            print("Meeste keskmine palk on", meeste_keskmine)
+            print("Naiste keskmine palk on", naiste_keskmine)
+            print("Meeste kõrgeim palk on", meeste_max)
+            print("Naiste kõrgeim palk on", naiste_max)
+            if meeste_keskmine > naiste_keskmine and meeste_max > naiste_max:
+                print("Firmas toimub diskrimineerimist soo järgi")
+            else:
+                print("Firmas ei toimu diskrimineerimist soo järgi")
 
+            palgad()
+
+# 16. TÃ¤ringud
+# 	Kasutaja vÃµistleb kahe tÃ¤ringuga arvuti vastu. Kasutaja teeb pakkumise ning suurima tÃ¤ringupunktisumma vÃµitja saab laual oleva raha endale juurde. MÃ¤ng kestab kuni kummalgi on raha otsas.
+# 	(Vihjed: kÃ¼si kasutajalt nime, kuva pidevalt konto seisu ja tÃ¤ringuviskeid, kasutajate raha hulga mÃ¤ngu alguses mÃ¤Ã¤rad sina)
+     
+                def taringu_mang():
+                raha = 100
+                arvuti_raha = 100
+                nimi = input("Sisesta oma nimi: ")
+                print("Tere", nimi, "!")
+                while raha > 0 and arvuti_raha > 0:
+                    print("Sul on", raha, "€ ja arvutil on", arvuti_raha, "€.")
+                    panus = int(input("Sisesta panus: "))
+                    if panus > raha:
+                        print("Sul ei ole nii palju raha!")
+                        continue
+                    enter = input("Vajuta enterit, et täringuid veeretada.")
+                    taring = random.randint(1,6)
+                    print("Sul tuli", taring, "silma.")
+                    arvuti_taring = random.randint(1,6)
+                    print("Arvutil tuli", arvuti_taring, "silma.")
+                    if taring > arvuti_taring:
+                        print("Sina võitsid!")
+                        raha += panus
+                        arvuti_raha -= panus
+                    elif taring < arvuti_taring:
+                        print("Arvuti võitis!")
+                        raha -= panus
+                        arvuti_raha += panus
+                    else:
+                        print("Viik!")  
+                        taringu_mang()
--- test.py
+++
@@ -1,8 +0,0 @@
-kasutaja_arv = int(input("Sisesta arv: "))
-
-if kasutaja_arv == 0:
-    print("Sisestasid nulli. Palun sisesta mõni muu arv.")
-elif kasutaja_arv % 2 == 0:
-    print("Sisestatud arv on paaris.")
-else:
-    print("Sisestatud arv on paaritu.")
