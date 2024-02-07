138ded40c47d6c937cfe9d2a7ce98f8289732fda
Kr1ss22 <kristjan.mustkivi@hkhk.edu.ee>
2024-02-07 13:03:03 +0200

    dfaf

--- Kontroltöö.py
+++ Kontroltöö.py
@@ -4,6 +4,7 @@
 #1. Korrutamise kontrollimine
 
 import random
+import random
 
 õiged_vastused = 0
 küsimuste_arv = 10
@@ -182,18 +183,31 @@ def taringu_mang():
                 taring = random.randint(1,6)
                 print("Sul tuli", taring, "silma.")
             
-                arvuti_taring = random.randint(1,6)
-                print("Arvutil tuli", arvuti_taring, "silma.")
-                if taring > arvuti_taring:
+                def taringu_mang():
+                    raha = 100
+                    arvuti_raha = 100
+                    nimi = input("Sisesta oma nimi: ")
+                    print("Tere", nimi, "!")
+                    while raha > 0 and arvuti_raha > 0:
+                        print("Sul on", raha, "€ ja arvutil on", arvuti_raha, "€.")
+                        panus = int(input("Sisesta panus: "))
+                        if panus > raha:
+                            print("Sul ei ole nii palju raha!")
+                            continue
+                        enter = input("Vajuta enterit, et täringuid veeretada.")
+                        taring = random.randint(1,6)
+                        print("Sul tuli", taring, "silma.")
+                        arvuti_taring = random.randint(1,6)
+                        print("Arvutil tuli", arvuti_taring, "silma.")
+                        if taring > arvuti_taring:
                             print("Sina võitsid!")
                             raha += panus
                             arvuti_raha -= panus
-                elif taring < arvuti_taring:
+                        elif taring < arvuti_taring:
                             print("Arvuti võitis!")
-                raha -= panus
-                arvuti_raha += panus
-                else:
-                print("Viik!")  
-                            
-                
+                            raha -= panus
+                            arvuti_raha += panus
+                        else:
+                            print("Viik!")  
+                                
                 taringu_mang()
