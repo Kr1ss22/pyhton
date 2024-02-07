b13f605e84da7ee86afd2f2dcf07a1c817adcf00
Kr1ss22 <kristjan.mustkivi@hkhk.edu.ee>
2024-02-07 14:33:51 +0200

    dada

---
+++ TkinterUl3.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+aken = Tk()
+
+Tekst = Label(aken, text="Nimi: Kristjan Mustkivi\nRühm: IT-23\n2024", font="Tahoma 16 bold italic", foreground="red", background="black")
+Tekst.pack()
+
+aken.resizable(0, 0)
+
+
+aken.mainloop()
+
